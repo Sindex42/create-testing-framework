937ed538530abbda4e690cf3c71ee32f6bbb45b4
Connor Kam-Cheong <connorkc42@gmail.com>
2019-01-25 12:29:03 +0000

    Add example tests for throwing errors

--- spec/SquareSpec.js
+++ spec/SquareSpec.js
@@ -1,13 +1,22 @@
-describe('Square', function () {
-  describe('#area', function () {
-    describe('calculates the area', function () {
-      it('passing test', function () {
+describe('Square', () => {
+  describe('throwing an error on instantiating', () => {
+    it('a square with NaN', () => {
+      expect(() => { new Square('a') }).toThrowError('argument is not a number')
+    })
+
+    it('a square with a negative integer', () => {
+      expect(() => { new Square(-1) }).toThrowError('size should be a positive number')
+    })
+  })
+
+  describe('#area', () => {
+    describe('calculates the area', () => {
+      it('passing test', () => {
         let square = new Square(4)
         expect(square.area()).toEqual(16)
       })
 
-
-      it('failing test', function () {
+      it('failing test', () => {
         let square = new Square(4)
         expect(square.area()).toEqual(17)
       })
